7cf0178e4f52e0af31299f794c523fa71c1352a2
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-05-31 12:50:09 +0200

    removed extra comma

--- model_train.py
+++ model_train.py
@@ -5,13 +5,14 @@ from torch import nn
 from collections import OrderedDict
 
 # YOUR CODE HERE
-   
+
+
 def model_train(input_size, hidden_layer1, hidden_layer2, output_size):
     model = nn.Sequential(nn.Linear(input_size, hidden_layer1),
-                        nn.ReLU(),
-                        nn.Linear(hidden_layer1, hidden_layer2),
-                        nn.ReLU(),
-                        nn.Linear(hidden_layer2, output_size),
-                        nn.Softmax(dim=1)))
+                          nn.ReLU(),
+                          nn.Linear(hidden_layer1, hidden_layer2),
+                          nn.ReLU(),
+                          nn.Linear(hidden_layer2, output_size),
+                          nn.Softmax(dim=1))
 
     return model
